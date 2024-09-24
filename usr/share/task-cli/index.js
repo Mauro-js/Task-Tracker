d8a14ac7624613948ed864a2a08f9971b08df821
Mauro <mnievas@antel.net.uy>
2024-09-23 23:58:48 -0300

    actions

--- usr/share/task-cli/index.js
+++ usr/share/task-cli/index.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+// Obtener el directorio actual
+const currentDirectory = __dirname;
+
+
 // Verificar si se pasó al menos un segundo argumento
 if (process.argv.length < 3) {
   console.log('No se pasó un segundo argumento.');
@@ -5,16 +12,22 @@ if (process.argv.length < 3) {
 
   switch(process.argv[2]){
     case "add":
+      console.log("ACCION ADD");
     break;
     case "update":
+      console.log("ACCION UPDATE");
     break;
     case "delete":
+      console.log("ACCTION DELETE");
     break;
     case "mark-in-progress":
+      console.log("ACCTION MARK");
     break;
     case "mark-done":
+      console.log("ACCITON MARK-DONE");
     break;
     case "list":
+      console.log("ACCTION LIST");
     break;
   }
 }
