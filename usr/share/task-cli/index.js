c251d802d6ee4f3b5cce13a021e14a14a9cbace4
Mauro <mnievas@antel.net.uy>
2024-09-23 23:25:40 -0300

    cambio de js

--- usr/share/task-cli/index.js
+++ usr/share/task-cli/index.js
@@ -1,4 +1,7 @@
-
-
-
-console.log("hola");
+// Verificar si se pasó al menos un segundo argumento
+if (process.argv.length < 4) {
+  console.log('No se pasó un segundo argumento.');
+} else {
+  // Imprimir el segundo argumento
+  console.log('El segundo argumento es:', process.argv[3]);
+}
