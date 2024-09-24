51790686f49c8548cc22e78f49a55a53f775ceb8
Mauro <mnievas@antel.net.uy>
2024-09-23 23:38:42 -0300

    switch case javascrit

--- usr/share/task-cli/index.js
+++ usr/share/task-cli/index.js
@@ -1,7 +1,20 @@
 // Verificar si se pasó al menos un segundo argumento
-if (process.argv.length < 4) {
+if (process.argv.length < 3) {
   console.log('No se pasó un segundo argumento.');
 } else {
-  // Imprimir el segundo argumento
-  console.log('El segundo argumento es:', process.argv[3]);
+
+  switch(process.argv[2]){
+    case "add":
+    break;
+    case "update":
+    break;
+    case "delete":
+    break;
+    case "mark-in-progress":
+    break;
+    case "mark-done":
+    break;
+    case "list":
+    break;
+  }
 }
